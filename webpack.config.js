636dee6f7d63f2b6e21595a357e451ff40c3bbf3
卡晨 <guangbo.hgb@antfin.com>
2018-09-14 15:42:56 +0800

    output management

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     index: "./src/index.tsx",
     widget: './src/widget.tsx',
   },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, 'dist')
