d25537d6a9314a9ee76d530e17393bcda41cef05
卡晨 <guangbo.hgb@antfin.com>
2018-09-18 10:49:36 +0800

    remove unused packages and configurations

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,9 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 // TODO add tree shaking feature
 
 module.exports = {
+
   entry: {
-    index: "./src/index.tsx",
+    index: './src/index.tsx',
     // widget: './src/widget.tsx',
   },
 
@@ -23,12 +24,11 @@ module.exports = {
   ],
 
   output: {
-    filename: "[name].bundle.js",
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: 'source-map',
 
   devServer: {
     contentBase: './dist',
@@ -36,24 +36,14 @@ module.exports = {
   },
 
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: ['.ts', '.tsx', '.js', '.json']
   },
 
   module: {
     rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       {
         test: /\.tsx?$/,
-        use: [
-          // {
-          //   loader: 'babel-loader',
-          // },
-          {
-            // loader: "ts-loader",
-            loader: "awesome-typescript-loader"
-          }
-        ],
+        loader: 'awesome-typescript-loader',
         exclude: /node_modules/
       },
       {
@@ -64,16 +54,8 @@ module.exports = {
         ]
       },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      // { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      // { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
     ]
-  },
+  }
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  // externals: {
-  //   "react": "React",
-  //   "react-dom": "ReactDOM"
-  // }
 };
