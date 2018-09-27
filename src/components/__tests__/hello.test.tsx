e82ef31847c03b36291de613f53544e3b3ee7d21
卡晨 <guangbo.hgb@antfin.com>
2018-09-27 11:14:29 +0800

    add jest setup file

--- src/components/__tests__/hello.test.tsx
+++ src/components/__tests__/hello.test.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Adapter from 'enzyme-adapter-react-16'
-import Enzyme from 'enzyme'
 import {Hello} from '../Hello'
+import {shallow} from 'enzyme'
 
-Enzyme.configure({ adapter: new Adapter() })
 
 it('renders correctly', () => {
-  const tree = Enzyme.shallow(
+  const tree = shallow(
     <Hello compiler="TypeScript 123" framework="React" />,
   )
   expect(tree).toMatchSnapshot()
